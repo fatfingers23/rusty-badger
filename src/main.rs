5930355fb947cb52a1ddcf2445bc77057d19532b
Bailey Townsend <baileytownsend2323@gmail.com>
2024-08-29 22:11:31 -0500

    working demo with new deps

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,34 @@
 
 #![no_std]
 #![no_main]
-
 use control_driver::setup_control;
-use defmt::*;
+use embassy_embedded_hal::shared_bus::asynch::spi::SpiDevice;
 use embassy_executor::Spawner;
-use embassy_time::{Duration, Timer};
+use embassy_rp::gpio;
+use embassy_rp::gpio::Input;
+use embassy_rp::spi::Spi;
+use embassy_rp::spi::{self};
+use embassy_sync::blocking_mutex::raw::NoopRawMutex;
+use embassy_sync::mutex::Mutex;
+use embassy_time::Delay;
+use embedded_graphics::{
+    mono_font::{ascii::*, MonoTextStyle},
+    pixelcolor::BinaryColor,
+    prelude::*,
+    primitives::{PrimitiveStyle, Rectangle},
+};
+use embedded_text::{
+    alignment::HorizontalAlignment,
+    style::{HeightMode, TextBoxStyleBuilder},
+    TextBox,
+};
+use gpio::{Level, Output, Pull};
+use uc8151::asynch::Uc8151;
+use uc8151::LUT;
+use uc8151::WIDTH;
 use {defmt_rtt as _, panic_probe as _};
-
 mod control_driver;
+
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
@@ -20,16 +40,83 @@ async fn main(spawner: Spawner) {
     )
     .await;
 
-    let delay = Duration::from_secs(1);
+    let miso = p.PIN_16;
+    let mosi = p.PIN_19;
+    let clk = p.PIN_18;
+    let dc = p.PIN_20;
+    let cs = p.PIN_17;
+    let busy = p.PIN_26;
+    let reset = p.PIN_21;
+    let power = p.PIN_10;
 
-    loop {
-        info!("led on!");
-        control.gpio_set(0, true).await;
-        Timer::after(delay).await;
+    let btn_up = p.PIN_15;
+    let btn_down = p.PIN_11;
+    let btn_a = p.PIN_12;
+    let btn_b = p.PIN_13;
+    let btn_c = p.PIN_14;
 
-        info!("led off!");
-        control.gpio_set(0, false).await;
+    let reset = Output::new(reset, Level::Low);
+    let _power = Output::new(power, Level::Low);
 
-        Timer::after(delay).await;
-    }
+    let dc = Output::new(dc, Level::Low);
+    let cs = Output::new(cs, Level::High);
+    let busy = Input::new(busy, Pull::Up);
+
+    let mut _btn_up = Input::new(btn_up, Pull::Up);
+    let mut _btn_down = Input::new(btn_down, Pull::Up);
+    let mut _btn_a = Input::new(btn_a, Pull::Up);
+    let mut _btn_b = Input::new(btn_b, Pull::Up);
+    let mut _btn_c = Input::new(btn_c, Pull::Up);
+
+    let spi = Spi::new(
+        p.SPI0,
+        clk,
+        mosi,
+        miso,
+        p.DMA_CH1,
+        p.DMA_CH2,
+        spi::Config::default(),
+    );
+    let spi_bus: Mutex<NoopRawMutex, _> = Mutex::new(spi);
+    let spi_dev = SpiDevice::new(&spi_bus, cs);
+    let mut display = Uc8151::new(spi_dev, dc, busy, reset, Delay);
+    display.reset().await;
+
+    // Initialise display. Using the default LUT speed setting
+    let _ = display.setup(LUT::Internal).await;
+
+    // Note we're setting the Text color to `Off`. The driver is set up to treat Off as Black so that BMPs work as expected.
+    let character_style = MonoTextStyle::new(&FONT_9X18_BOLD, BinaryColor::Off);
+    let textbox_style = TextBoxStyleBuilder::new()
+        .height_mode(HeightMode::FitToText)
+        .alignment(HorizontalAlignment::Center)
+        .paragraph_spacing(6)
+        .build();
+
+    // Bounding box for our text. Fill it with the opposite color so we can read the text.
+    let bounds = Rectangle::new(Point::new(10, 10), Size::new(WIDTH - 157, 0));
+    bounds
+        .into_styled(PrimitiveStyle::with_fill(BinaryColor::On))
+        .draw(&mut display)
+        .unwrap();
+
+    // Create the text box and apply styling options.
+    let text = "Written In\nRust!";
+    let text_box = TextBox::with_textbox_style(text, bounds, character_style, textbox_style);
+
+    // Draw the text box.
+    text_box.draw(&mut display).unwrap();
+    let _ = display.update().await;
+    control.gpio_set(0, true).await;
+
+    // loop {
+    //     info!("led on!");
+    //     control.gpio_set(0, true).await;
+    //     Timer::after(delay).await;
+
+    //     info!("led off!");
+    //     control.gpio_set(0, false).await;
+
+    //     Timer::after(delay).await;
+    // }
 }
